3c7779c600fe10c2bcb9b0a699da9565ac5eb483
tonklis <benjamin.hernandez@in2teck.com>
2018-11-22 13:35:01 -0600

    added to S for script to run

--- scripts/calculate_comissions.rb
+++ scripts/calculate_comissions.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require_relative "../config/environment"
 
-period_start = ARGV[0].to_time
-period_end = ARGV[1].to_time
+period_start = ARGV[0].to_s.to_time
+period_end = ARGV[1].to_s.to_time
 
 if period_start == period_end - 1.week
   Payment.calculate_weekly_comissions period_start, period_end
