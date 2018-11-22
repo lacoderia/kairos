6f5f9e06c7a74280780b60378ab37c5a9353eb80
tonklis <benjamin.hernandez@in2teck.com>
2018-11-22 13:46:56 -0600

    Fixed payments with time zone

--- scripts/calculate_comissions.rb
+++ scripts/calculate_comissions.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 require_relative "../config/environment"
 
-period_start = ARGV[0].to_s.to_time
-period_end = ARGV[1].to_s.to_time
+period_start = ARGV[0].to_s.in_time_zone
+period_end = ARGV[1].to_s.in_time_zone
 
 if period_start == period_end - 1.week
   Payment.calculate_weekly period_start, period_end
