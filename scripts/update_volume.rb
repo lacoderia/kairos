5add42e97e2871932528e9b9568eb42d9ce1f027
tonklis <benjamin.hernandez@in2teck.com>
2018-12-04 16:52:27 -0600

    Added logger to update_volume

--- scripts/update_volume.rb
+++ scripts/update_volume.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/env ruby
 require_relative "../config/environment"
 
-period_start = Time.zone.now.beginning_of_month
-period_end = period_start + 1.month
+logger = Logger.new('/home/deploy/kairos/log/update_summaries.log', 5, 1024000)
 
-User.update_summaries period_start, period_end
+begin
+  period_start = Time.zone.now.beginning_of_month
+  period_end = period_start + 1.month
+  User.update_summaries period_start, period_end
+  logger.info("Successfully completed")  
+rescue Exception => e
+  logger.error("Error - #{e.message}")
+end
