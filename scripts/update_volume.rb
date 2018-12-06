ba80bd2b78e9fc3855d506bb1dac37bb12084ab4
tonklis <benjamin.hernandez@in2teck.com>
2018-12-06 13:18:45 -0600

    Added processing of last month orders that are inside the first 10 days of the current month

--- scripts/update_volume.rb
+++ scripts/update_volume.rb
@@ -7,6 +7,13 @@ begin
   period_start = Time.zone.now.beginning_of_month
   period_end = period_start + 1.month
   User.update_summaries period_start, period_end
+
+  if Time.zone.now.day <= 10
+    period_start = period_start - 1.month
+    period_end = period_start + 1.month
+    User.update_summaries period_start, period_end
+  end
+
   logger.info("Successfully completed")  
 rescue Exception => e
   logger.error("Error - #{e.message}")
