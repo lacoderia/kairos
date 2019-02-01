25157e2c12f462f210f8831ad7716bcd4acccb5d
tonklis <benjamin.hernandez@in2teck.com>
2019-02-01 16:39:33 -0600

    Added rank

--- scripts/omein_payment_for_users.rb
+++ scripts/omein_payment_for_users.rb
@@ -5,7 +5,7 @@ PERIOD_START = ARGV[0].to_s.in_time_zone
 PERIOD_END = ARGV[1].to_s.in_time_zone
 
 CSV.open("payments_omein_#{ARGV[0]}_#{ARGV[1]}.csv", "wb") do |csv|
-  csv << ["ID OMEIN", "NOMBRE", "APELLIDO", "EMAIL", "$ NIVEL 1", "NIVEL 1 IDS", "$ NIVEL 2", "NIVEL 2 IDS", "$ NIVEL 3", "NIVEL 3 IDS", "$ NIVEL 4", "NIVEL 4 IDS", "$ NIVEL 5", "NIVEL 5 IDS", "$ NIVEL 6", "NIVEL 6 IDS", "$ NIVEL 7", "NIVEL 7 IDS", "$ NIVEL 8", "NIVEL 8 IDS", "$ NIVEL 9", "NIVEL 9 IDS", "$ TOTAL" ]
+  csv << ["ID OMEIN", "NOMBRE", "APELLIDO", "EMAIL", "RANGO", "$ NIVEL 1", "NIVEL 1 IDS", "$ NIVEL 2", "NIVEL 2 IDS", "$ NIVEL 3", "NIVEL 3 IDS", "$ NIVEL 4", "NIVEL 4 IDS", "$ NIVEL 5", "NIVEL 5 IDS", "$ NIVEL 6", "NIVEL 6 IDS", "$ NIVEL 7", "NIVEL 7 IDS", "$ NIVEL 8", "NIVEL 8 IDS", "$ NIVEL 9", "NIVEL 9 IDS", "$ TOTAL" ]
   users = User.joins(:orders).where("orders.created_at >= ? AND orders.created_at < ?", PERIOD_START, PERIOD_END).order("external_id desc")
     
   users.uniq.each do |user|
@@ -81,6 +81,8 @@ CSV.open("payments_omein_#{ARGV[0]}_#{ARGV[1]}.csv", "wb") do |csv|
     end
 
     total = level_1 + level_2 + level_3 + level_4 + level_5 + level_6 + level_7 + level_8 + level_9 
+    
+    summary = user.summaries.where(period_start: PERIOD_START, period_end: PERIOD_END).first
         
     level_1_ids = level_1_ids*"," 
     level_2_ids = level_2_ids*"," 
@@ -92,7 +94,7 @@ CSV.open("payments_omein_#{ARGV[0]}_#{ARGV[1]}.csv", "wb") do |csv|
     level_8_ids = level_8_ids*"," 
     level_9_ids = level_9_ids*"," 
 
-    user_txt = ["#{user.external_id}", "#{user.first_name}", "#{user.last_name}", "#{user.email}", "#{level_1.round(2)}", "#{level_1_ids}", "#{level_2.round(2)}", "#{level_2_ids}", "#{level_3.round(2)}", "#{level_3_ids}","#{level_4.round(2)}", "#{level_4_ids}", "#{level_5.round(2)}", "#{level_5_ids}", "#{level_6.round(2)}", "#{level_6_ids}", "#{level_7.round(2)}", "#{level_7_ids}", "#{level_8.round(2)}", "#{level_8_ids}", "#{level_9.round(2)}", "#{level_9_ids}", "#{total.round(2)}"]
+    user_txt = ["#{user.external_id}", "#{user.first_name}", "#{user.last_name}", "#{user.email}", "#{summary.rank}", "#{level_1.round(2)}", "#{level_1_ids}", "#{level_2.round(2)}", "#{level_2_ids}", "#{level_3.round(2)}", "#{level_3_ids}","#{level_4.round(2)}", "#{level_4_ids}", "#{level_5.round(2)}", "#{level_5_ids}", "#{level_6.round(2)}", "#{level_6_ids}", "#{level_7.round(2)}", "#{level_7_ids}", "#{level_8.round(2)}", "#{level_8_ids}", "#{level_9.round(2)}", "#{level_9_ids}", "#{total.round(2)}"]
     csv << user_txt
   end
 end
