5bc590e95fdbe65ac1c244bb877c58c90f2f9005
tonklis <benjamin.hernandez@in2teck.com>
2019-05-22 18:28:34 -0500

    Added payment api in charge job

--- app/jobs/charge_fee_job.rb
+++ app/jobs/charge_fee_job.rb
@@ -4,6 +4,7 @@ class ChargeFeeJob < ActiveJob::Base
 
   def perform(user, company, order)
     begin
+      payment_api = OpenpayHelper.new(company)      
       charge_fee_hash = payment_api.charge_fee(user.get_openpay_id(company), order.total_price, order.description, nil)
     rescue Exception => e
       Email.create(user: user, email_status: e.message, email_type: "Error charging fee - order #{order.openpay_id}")
